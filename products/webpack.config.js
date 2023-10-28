fadb263ca473437c613787d5640c59b56a4ae7dc
mrinalgithub <mrinalgithub@gmail.com>
2023-10-28 20:05:11 +0530

    chore: dev setup completed for products

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template:'./public/index.html'
+    })
+]
+
 module.exports = {
-    mode:"development"
+    mode:"development",
+    devServer:{
+        port:8080
+    },
+    plugins
 }
